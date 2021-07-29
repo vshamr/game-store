ba2b7864400e1911ad32fadf086375ee2f5e7fa3
vshamr <vero94@mail.ru>
2021-07-29 11:36:54 +0300

    update layout

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -4,19 +4,22 @@ import "./styles.css";
 
 const Footer: React.FC = () => (
   <>
-    <footer>
+  <footer>
+    <div className="footer_container">
       <h2 className="footer_title">Incredible convenient</h2>
       <ul className="footer_image">
         {
           FOOTER_DATA.map((
             { href, img }) => (
-              <li key={href}>
-                <a href={href}>{img}</a>
-              </li>
-            ))}
+            <li key={href}>
+              <a href={href}>{img}</a>
+            </li>
+          ))}
       </ul>
-    </footer>
-  </>
-);
+  </div>
+  </footer>
+</>
+)
+;
 
 export default Footer;
