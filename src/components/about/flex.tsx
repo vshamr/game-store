cc87626cb6c38a49afe96c99aa748375e4df6175
vshamr <vero94@mail.ru>
2021-09-09 13:43:52 +0300

    optimitization

---
+++ src/components/about/flex.tsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+const StyledFlex = styled.div`
+  display: flex;
+  flex-direction: ${(props) => props.direction || "row"};
+  align-items: ${(props) => props.align || "stretch"};
+  justify-content: ${(props) => props.justify || "stretch"};
+  margin: ${({ margin }) => margin || "0"};
+`;
+
+const Flex = (props) => <StyledFlex {...props} />;
+
+export default Flex;
