c8783f944360bfdf421d36e9287b4bcadb085539
vshamr <vero94@mail.ru>
2021-09-19 23:39:07 +0300

    fix AuthorizedUser page

--- src/components/loginization/authorizedUser.tsx
+++ src/components/loginization/authorizedUser.tsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
-import { FaRegUser, ImExit, RiAdminFill } from "react-icons/all";
+import { FaRegUser, ImExit } from "react-icons/all";
 
 import "../header/styles.css";
 import { Routes } from "@/constants/Routes";
@@ -9,8 +9,6 @@ import { ReducersType } from "@/redux/redux-store";
 
 function AuthorizedUser(): JSX.Element {
   const userName = useSelector((state: ReducersType) => state.userPage.userName);
-  const isAdmin = useSelector((state: ReducersType) => state.userPage.isAdmin);
-  const authorizedUser = useSelector((state: ReducersType) => state.userPage.isLoggedIn);
   const dispatch = useDispatch();
 
   const handleLogOut = () => dispatch(logOut());
@@ -20,13 +18,6 @@ function AuthorizedUser(): JSX.Element {
       <Link to={Routes.PROFILE_PAGE}>
         <FaRegUser /> {userName}
       </Link>
-      {authorizedUser && !isAdmin ? (
-        <Link to={Routes.ADMIN}>
-          <div className="admin-icon">
-            <RiAdminFill /> admin
-          </div>
-        </Link>
-      ) : null}
       <span onClick={handleLogOut}>
         <Link to={Routes.HOME}>
           <ImExit />
